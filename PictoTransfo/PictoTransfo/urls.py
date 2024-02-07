39b47825db6c21ddee69d3a0f7222f9306003300
Alexis1663 <92861778+Alexis1663@users.noreply.github.com>
2024-02-07 11:49:19 +0100

    add phot upload

--- PictoTransfo/PictoTransfo/urls.py
+++ PictoTransfo/PictoTransfo/urls.py
@@ -16,8 +16,14 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from .settings import MEDIA_ROOT, DEBUG, MEDIA_URL
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('app.urls'))
 ]
+
+if DEBUG:
+    urlpatterns += static(
+    MEDIA_URL, document_root=MEDIA_ROOT)
