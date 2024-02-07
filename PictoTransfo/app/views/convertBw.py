f5767b67249d17396873f1de5b907613234a59fc
Audric-S <sabatieraudric@gmail.com>
2024-02-07 20:51:21 +0100

    :memo: add style on bw page

--- PictoTransfo/app/views/convertBw.py
+++ PictoTransfo/app/views/convertBw.py
@@ -8,6 +8,8 @@ def convert_to_bw(image_path):
     bw_img = img.convert("1")
     return bw_img
 
+    #FAIRE VERIF SI IMG EXIST SINON RAISE EXCEPTION
+
 def hello_world(request):
     if request.method == 'GET':
         image_path = 'media/image.jpg'
@@ -17,4 +19,3 @@ def hello_world(request):
         bw_img.save('media/image_bw.jpg')
 
         return render(request, 'convert-bw.template.html')
-
